97b576e07e63370700404453ba93eefc9ec64203
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-01-19 17:40:22 -0800

    Adding initial GUI layout

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,34 @@
 package com.example;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
 
 public class PrimaryController {
+    @FXML
+    private TextField searchField;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label searchResultsLabel;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        Window owner = submitButton.getScene().getWindow();
+        if(searchField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter a search term (author, title, genre).");
+            return;
+        }
+
+        AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", 
+                "Welcome " + searchField.getText());
     }
 }
